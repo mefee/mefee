a8a6996163d5f0b0c0d7763ef08533d525d58133
Harry Peirse <peirse@gmail.com>
2020-03-28 14:49:51 +0000

    Initial Form made

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,16);
+});
+
 window.onload = function() {
     $('#fieldset-btn').on('click', function() {
         $('#fieldset').hide()
@@ -8,4 +14,7 @@ window.onload = function() {
         $('#results').hide()
         $('#fieldset').show()
     })
+
+    console.log(new this.Date().toDateInputValue())
+    $('#datetime').val(new Date().toDateInputValue())
 }
