3a1d5201ddee6692e8c50732ac313d948e6d74eb
Harry Peirse <peirse@gmail.com>
2020-03-28 17:15:48 +0000

    temperature toggling updates chart

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ Date.prototype.toDateInputValue = (function () {
     return local.toJSON().slice(0, 16);
 });
 
+function farenheitToCelsius(farenheit) {
+    return Math.round(100 * ((farenheit - 32) * 5/9)) / 100
+}
+
+function celsiusToFarenheit(celsius) {
+    return Math.round(100 * ((celsius * 9/5) + 32)) / 100
+}
+
+var temperatureUnit = "f"
+var chart
+
 /*
 UserContext
 {
@@ -44,13 +55,42 @@ window.onload = function () {
 
     $('#datetime').val(new Date().toDateInputValue())
 
+    $('input[type=radio][name=inputTemperatureUnit]').change(function() {
+        if (temperatureUnit == 'c' && this.value == 'f') {
+            chart.data.datasets[0].data.forEach(function(e) { e.y = celsiusToFarenheit(e.y) })
+            temperatureUnit = 'f'
+            $('#displayFarenheit').prop('checked', true)
+        }
+        else if (temperatureUnit == 'f' && this.value == 'c') {
+            chart.data.datasets[0].data.forEach(function(e) { e.y = farenheitToCelsius(e.y) })
+            temperatureUnit = 'c'
+            $('#displayCelsius').prop('checked', true)
+        }
+        chart.update()
+    });
+
+    $('input[type=radio][name=displayTemperatureUnit]').change(function() {
+        if (temperatureUnit == 'c' && this.value == 'f') {
+            chart.data.datasets[0].data.forEach(function(e) { e.y = celsiusToFarenheit(e.y) })
+            temperatureUnit = 'f'
+            $('#inputFarenheit').prop('checked', true)
+        }
+        else if (temperatureUnit == 'f' && this.value == 'c') {
+            chart.data.datasets[0].data.forEach(function(e) { e.y = farenheitToCelsius(e.y) })
+            temperatureUnit = 'c'
+            $('#inputCelsius').prop('checked', true)
+        }
+        chart.update()
+    });
+
     var chartContext = document.getElementById('chart').getContext('2d');
-    var chart = new Chart(chartContext, {
+    chart = new Chart(chartContext, {
         type: 'line',
         data: {
             datasets: [{
                 borderColor: '#55DD55',
                 fill: false,
+                showLine: false,
                 data: getDataPoints()
             }]
         },
@@ -61,11 +101,12 @@ window.onload = function () {
             },
             scales: {
                 xAxes: [{
-                    type: 'time',
-                    display: true
+                    type: 'time'
                 }],
                 yAxes: [{
-                    display: true
+                    ticks: {
+                        stepSize: 0.5
+                    }
                 }]
             }
         }
